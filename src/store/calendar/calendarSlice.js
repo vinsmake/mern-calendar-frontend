0183691e0390e0e735268a2c2580c66983e9b3bf
vinsmake <ps.enrique.e@gmail.com>
2024-06-04 14:08:24 -0600

    Project updated

--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -27,25 +27,6 @@ export const calendarSlice = createSlice({
     reducers: {
         onSetActiveEvent: ( state, { payload }) => {
             state.activeEvent = payload;
-        },
-        onAddNewEvent: ( state, { payload }) => {
-            state.events.push( payload );
-            state.activeEvent = null;
-        },
-        onUpdateEvent: ( state, { payload } ) => {
-            state.events = state.events.map( event => {
-                if ( event._id === payload._id ) {
-                    return payload;
-                }
-
-                return event;
-            });
-        },
-        onDeleteEvent: ( state ) => {
-            if ( state.activeEvent ) {
-                state.events = state.events.filter( event => event._id !== state.activeEvent._id );
-                state.activeEvent = null;
-            }
         }
     }
 });

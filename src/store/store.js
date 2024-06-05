03fcb837b33cb840f4d9e12ceaf53e4d610e3c48
vinsmake <ps.enrique.e@gmail.com>
2024-06-05 12:31:41 -0600

    Project updated

--- src/store/store.js
+++ src/store/store.js
@@ -7,5 +7,8 @@ export const store = configureStore({
     reducer: {
         calendar: calendarSlice.reducer,
         ui: uiSlice.reducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware ({
+        serializableCheck: false
+    })
 })
